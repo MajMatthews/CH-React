dfb53f38b939d8d90796cda7f5b9e889e9a897ac
Craig Matthews <mrcraigmatthews@hotmail.co.uk>
2017-11-20 07:19:15 +0000

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+//This component handles the App template on every page
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <div className="container-fluid">
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ABoutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>React, Redux and React Router and a varaity of other helpful stuff</p>
+      </div>
+    );
+  }
+}
+
